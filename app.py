37eed5adaa762ee036e2a3392067bdc2a4d63b17
Vahid Fazel-Rezai <vahidfazelrezai@gmail.com>
2016-07-09 17:29:12 -0700

    asdfasdf

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, request
+import requests
+import urllib
 
 app = Flask(__name__)
 
+s = requests.Session()
+
 @app.route('/')
-def root():
+def index():
     return app.send_static_file('index.html')
 
-@app.route('/blah/')
-@app.route('/blah/<name>')
-def hello(name=None):
-    return render_template('hello.html', name=name)
+@app.route('/<url>')
+def browse(url=None):
+    print url
+    return url
+    # url = urllib.unquote(url).decode('utf8')
+    # return url.split(':')[0]
+    # r = s.get()
+    # return r.content
