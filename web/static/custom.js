c399e72bf75eae61f5e1e068887cb3df2fb5b8fa
Varun Mohan <varunkmohan@gmail.com>
2016-07-10 02:57:47 +0000

    asdf

--- web/static/custom.js
+++ web/static/custom.js
@@ -1,3 +1,25 @@
 window.onload = function() {
-    alert("poop");
+    alert(window.location);
+}
+
+
+window.onclick = function(e){
+    e = e || event;
+    var from = findParent('a',e.target || e.srcElement);
+    if (from){
+        /* it's a link, actions here */
+        alert(e.target);
+    }
+}
+//find first parent with tagName [tagname]
+function findParent(tagname,el){
+    if ((el.nodeName || el.tagName).toLowerCase()===tagname.toLowerCase()){
+        return el;
+    }
+    while (el = el.parentNode){
+        if ((el.nodeName || el.tagName).toLowerCase()===tagname.toLowerCase()){
+            return el;
+        }
+    }
+    return null;
 }
