8ada7188e290ae958a22dc5d2588b2876f83fe57
kimberli <kimberlizhong@gmail.com>
2016-07-09 19:16:57 -0700

    add custom style instead

--- web/processor/simplifier.py
+++ web/processor/simplifier.py
@@ -3,5 +3,11 @@ from htmlmin.minify import html_minify
 
 def simplify(html):
     soup = BeautifulSoup(html, 'html.parser')
+    [s.extract() for s in soup('script')]
+    [s.extract() for s in soup('link')]
+    [s.extract() for s in soup('img')]
+
+    soup.head.append(soup.new_tag("link", rel="stylesheet", type="text/css", href="/static/style.css"))
+
     minhtml = html_minify(str(soup))
     return minhtml
